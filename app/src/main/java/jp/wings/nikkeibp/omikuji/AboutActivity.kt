5f254bd81bb26dadb51951b944d2fca9a748928e
goo-Sourcetree <goo@users.noreply.github.com>
2020-04-01 20:33:37 +0900

    First commit

---
+++ app/src/main/java/jp/wings/nikkeibp/omikuji/AboutActivity.kt
@@ -0,0 +1,16 @@
+package jp.wings.nikkeibp.omikuji
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.about.*
+
+class AboutActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.about)
+
+        val info = packageManager.getPackageInfo(packageName, 0)
+        textView3.text = "Version " + info.versionName
+    }
+}
